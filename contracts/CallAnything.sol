89ebe2718df7bccfa0d286ae3afd574d68b6e69d
gnoparus <surapong@gmail.com>
2022-08-14 16:43:02 +0700

    Add fallback, receive function

--- contracts/CallAnything.sol
+++ contracts/CallAnything.sol
@@ -2,47 +2,48 @@
 pragma solidity ^0.8.8;
 
 contract CallAnything {
-    
-    // 
+    //
 
     address public s_someAddress;
     uint256 public s_amount;
 
-    function transfer(address someAddress, uint256 amount) public  {
-
-        // 
+    function transfer(address someAddress, uint256 amount) public {
+        //
         s_someAddress = someAddress;
         s_amount = amount;
-
     }
 
     function getSelectorOne() public pure returns (bytes4 selector) {
-        // 
+        //
         selector = bytes4(keccak256(bytes("transfer(address,uint256)")));
     }
 
     function getDataToCallTransfer(address someAddress, uint256 amount) public pure returns (bytes memory) {
-        // 
+        //
         return abi.encodeWithSelector(getSelectorOne(), someAddress, amount);
-
     }
 
-    function callTransferFunctionDirectly(address someAddress, uint256 amount) public returns (bytes4, bool)  {
-
-        // 
+    function callTransferFunctionDirectly(address someAddress, uint256 amount) public returns (bytes4, bool) {
+        //
         // (bool success, bytes memory returnData) = address(this).call(getDataToCallTransfer(someAddress, amount));
-        (bool success, bytes memory returnData) = address(this).call(abi.encodeWithSelector(getSelectorOne(), someAddress, amount));
+        (bool success, bytes memory returnData) = address(this).call(
+            abi.encodeWithSelector(getSelectorOne(), someAddress, amount)
+        );
 
         return (bytes4(returnData), success);
     }
 
-    function callTransferFunctionDirectlySig(address someAddress, uint256 amount) public returns (bytes4, bool)  {
-
-        // 
+    function callTransferFunctionDirectlySig(address someAddress, uint256 amount) public returns (bytes4, bool) {
+        //
         // (bool success, bytes memory returnData) = address(this).call(getDataToCallTransfer(someAddress, amount));
-        (bool success, bytes memory returnData) = address(this).call(abi.encodeWithSignature("transfer(address,uint256)", someAddress, amount));
+        (bool success, bytes memory returnData) = address(this).call(
+            abi.encodeWithSignature("transfer(address,uint256)", someAddress, amount)
+        );
 
         return (bytes4(returnData), success);
     }
 
+    fallback() external payable {}
+
+    receive() external payable {}
 }
